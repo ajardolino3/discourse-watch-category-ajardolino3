389ad3c2b1c7e2513b0b03775238ef8c5ed2bccd
Christoph Holtermann <mail@c-holtermann.net>
2019-10-10 16:30:27 +0200

    Create option to silence subcategories for users in group

--- plugin.rb
+++ plugin.rb
@@ -15,6 +15,31 @@ module ::WatchCategory
     end
   end
 
+  def self.get_categories_recursive(category)
+    categories = [category]
+    subcategories = Category.where("categories.parent_category_id = ?", category.id)
+    
+    subcategories.each do |subcategory|
+      categories += get_categories_recursive(subcategory)
+    end
+    
+    return categories
+  end
+  
+  def self.mute_recursive_by_group(category_slug, group_name)
+    category = Category.find_by(slug: category_slug)
+    categories = self.get_categories_recursive(category)
+    group = Group.find_by_name(group_name)
+    return if category.nil? || group.nil?
+
+    categories.each do |subcategory|
+      group.users.each do |user|
+        muted_categories = CategoryUser.lookup(user, :muted).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:muted], subcategory.id) unless muted_categories.include?(subcategory.id) || user.staged
+      end
+    end
+  end
+  
   def self.watch_all(category_slug)
     category = Category.find_by(slug: category_slug)
     User.all.each do |user|
